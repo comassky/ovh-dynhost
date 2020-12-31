9167af546bf16ff217e774f49631313d8a5463b2
Hadrien JACQUOT <hjacquot.git@gmail.com>
2020-12-31 11:01:18 +0100

    Hide curl progress

--- dynhost.sh
+++ dynhost.sh
@@ -10,7 +10,7 @@ do
   if [ "$IP" ]; then
     if [ "$CURRENT_IP" != "$IP" ]; then
         echo "Updating $domain from $CURRENT_IP to $IP"
-        curl --user "${DYNHOST_LOGIN}:${DYNHOST_PASSWORD}" "http://www.ovh.com/nic/update?system=dyndns&hostname=${domain}&myip=${IP}"
+        curl -s --user "${DYNHOST_LOGIN}:${DYNHOST_PASSWORD}" "http://www.ovh.com/nic/update?system=dyndns&hostname=${domain}&myip=${IP}"
     else
        echo "No update required for $domain"
     fi
